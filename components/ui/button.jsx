4752625d25a394ac971f658afb7b041c37babdb6
HemangDudhat <hemangdudhat@gmail.com>
2025-12-29 12:58:54 +0530

    completed dashboard and contact pages including frontend and backend

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-[#312c9d] text-primary-foreground hover:bg-white border border-transparent hover:border-blue-900 hover:text-blue-900",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
